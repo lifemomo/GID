b02e05c6bbbcd26d49d65d460e0f729ba141a4aa
毛森林 <1285112478@qq.com>
2025-10-25 16:17:51 +0800

    Add files via upload

---
+++ generate_dataset.sh
@@ -0,0 +1,17 @@
+ts=500
+cluster=20
+#for dataset in Beauty ML100K ML1M Yelp Electronics Movies CDs Clothing Taobao LastFM
+for dataset in LastFM
+do
+#    for indexing in random sequential collaborative
+    for indexing in collaborative
+    do
+        python ./src/src_llama/generate_dataset.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+
+        python ./src/src_llama/generate_dataset_eval.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --mode validation --prompt seen:0 --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+
+        python ./src/src_llama/generate_dataset_eval.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --mode test --prompt seen:0 --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+
+        python ./src/src_llama/generate_dataset_eval.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --mode test --prompt unseen:0 --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+    done
+done
